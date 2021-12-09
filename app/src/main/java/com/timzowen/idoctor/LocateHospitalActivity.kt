d0e8ff3113efbcc22b1358c44ce6ac193db5f00e
TimzOwen <ktimothy.com>
2021-12-09 17:53:41 +0300

    updated the maps location

---
+++ app/src/main/java/com/timzowen/idoctor/LocateHospitalActivity.kt
@@ -0,0 +1,11 @@
+package com.timzowen.idoctor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LocateHospitalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_locate_hospital)
+    }
+}
---
+++ app/src/main/java/com/timzowen/idoctor/data/DataEmergency.kt
@@ -0,0 +1,8 @@
+package com.timzowen.idoctor.data
+
+import com.timzowen.idoctor.adapters.EmergencyAdapter
+
+class DataEmergency() {
+
+
+}
