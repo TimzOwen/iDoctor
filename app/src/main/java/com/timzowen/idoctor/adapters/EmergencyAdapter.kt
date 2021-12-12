a64437e81ad31bd7e97ee4dd0d2fb74c51050161
TimzOwen <ktimothy.com>
2021-12-12 22:05:56 +0300

    finished designing emergency activity

--- app/src/main/java/com/timzowen/idoctor/adapters/EmergencyAdapter.kt
+++ app/src/main/java/com/timzowen/idoctor/adapters/EmergencyAdapter.kt
@@ -1,4 +1,33 @@
 package com.timzowen.idoctor.adapters
 
-class EmergencyAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.timzowen.idoctor.R
+import com.timzowen.idoctor.model.EmergencyNumbers
+
+class EmergencyAdapter (private val mList : List<EmergencyNumbers>): RecyclerView.Adapter<EmergencyAdapter.ViewHolder>() {
+
+    class ViewHolder(ItemView : View) : RecyclerView.ViewHolder(ItemView){
+        val tvCounty :TextView = itemView.findViewById(R.id.tv_county)
+        val tvTown : TextView = itemView.findViewById(R.id.tv_emergency_hosp_town)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.emergeny_layout_list, parent,false)
+        return ViewHolder(view)
+    }
+
+    // get current number, county and hospital and map int to the holder
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentNumber = mList[position]
+        holder.tvCounty.text =currentNumber.county
+        holder.tvTown.text = currentNumber.town
+    }
+
+    // get the total list of numbers in the arrays
+    override fun getItemCount() =mList.size
 }
