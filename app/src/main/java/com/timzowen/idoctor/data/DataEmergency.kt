1261f99b20a9707b56f4b09264fb1f9b2b184ea4
TimzOwen <ktimothy.com>
2021-12-12 21:48:53 +0300

    fixed emergency adapters

--- app/src/main/java/com/timzowen/idoctor/data/DataEmergency.kt
+++ app/src/main/java/com/timzowen/idoctor/data/DataEmergency.kt
@@ -1,8 +1,17 @@
 package com.timzowen.idoctor.data
 
-import com.timzowen.idoctor.adapters.EmergencyAdapter
+import android.telephony.emergency.EmergencyNumber
+import com.timzowen.idoctor.model.EmergencyNumbers
 
 class DataEmergency() {
 
+    fun loadCalls() : List<EmergencyNumbers>{
+        return listOf(
+            EmergencyNumbers("Nairobi", "Kijabe","0722000000"),
+            EmergencyNumbers("Nakuru","Kabarak","0721000000"),
+            EmergencyNumbers("Baringo", "Mercy Hosp","0723000000"),
+        EmergencyNumbers("Mombasa","Mediheal","0724000000")
+        )
+    }
 
 }
---
+++ app/src/main/java/com/timzowen/idoctor/model/EmergencyNumbers.kt
@@ -0,0 +1,4 @@
+package com.timzowen.idoctor.model
+
+data class EmergencyNumbers(val county : String, val town: String, val call : String) {
+}
