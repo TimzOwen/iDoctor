dfeb69dd9840f85fe568938d2f92c80c03c08669
TimzOwen <ktimothy.com>
2021-12-13 10:58:31 +0300

    updated the emergency activity

--- app/src/main/java/com/timzowen/idoctor/EmergencyActivity.kt
+++ app/src/main/java/com/timzowen/idoctor/EmergencyActivity.kt
@@ -2,10 +2,23 @@ package com.timzowen.idoctor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.timzowen.idoctor.adapters.EmergencyAdapter
+import com.timzowen.idoctor.data.DataEmergency
 
 class EmergencyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_emergency)
+
+        //get the recycler view
+        val recyclerviewEmergency = findViewById<RecyclerView>(R.id.recycler_emergency)
+
+        //get all the numbers in the emergency data package
+        val dataNumbers = DataEmergency().loadCalls()
+
+        // map all the data in the adapter
+        recyclerviewEmergency.adapter = EmergencyAdapter(dataNumbers)
+
     }
 }
