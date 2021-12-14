d27e9505717407efb35796ae9b24a8b897593387
TimzOwen <ktimothy.com>
2021-12-14 18:47:16 +0300

    booking appointments

--- app/src/main/java/com/timzowen/idoctor/CalenderActivity.kt
+++ app/src/main/java/com/timzowen/idoctor/CalenderActivity.kt
@@ -1,11 +1,21 @@
 package com.timzowen.idoctor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.timzowen.idoctor.databinding.ActivityCalenderBinding
 
 class CalenderActivity : AppCompatActivity() {
+
+    private lateinit var calenderBinding: ActivityCalenderBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_calender)
+        calenderBinding = ActivityCalenderBinding.inflate(layoutInflater)
+        setContentView(calenderBinding.root)
+
+        calenderBinding.btnBookAppointment.setOnClickListener {
+            startActivity(Intent(this, BookAppointmentActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/timzowen/idoctor/LegalSupportActivity.kt
+++ app/src/main/java/com/timzowen/idoctor/LegalSupportActivity.kt
@@ -4,12 +4,9 @@ import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.ktx.Firebase
 import com.timzowen.idoctor.databinding.ActivityLegalSupportBinding
 import com.timzowen.idoctor.model.LegalCase
 
@@ -26,14 +23,12 @@ class LegalSupportActivity : AppCompatActivity() {
         binding = ActivityLegalSupportBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         binding.btnSubmitCase.setOnClickListener{
 
             progressDialog = ProgressDialog(this)
             progressDialog.setMessage("Reporting case.....")
             progressDialog.setCancelable(false)
             progressDialog.show()
-
 
             val hospName = binding.etHospName.text.toString()
             val caseType = binding.etTypeOfCase.text.toString()
