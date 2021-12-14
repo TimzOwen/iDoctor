571eb464db246a1ceffc4afc89312570c5564db4
TimzOwen <ktimothy.com>
2021-12-14 17:15:11 +0300

    finished review of Legal case reporting

--- app/src/main/java/com/timzowen/idoctor/LegalSupportActivity.kt
+++ app/src/main/java/com/timzowen/idoctor/LegalSupportActivity.kt
@@ -1,7 +1,9 @@
 package com.timzowen.idoctor
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -15,6 +17,8 @@ class LegalSupportActivity : AppCompatActivity() {
     private lateinit var binding : ActivityLegalSupportBinding
     private  lateinit var database : DatabaseReference
 
+    lateinit var progressDialog : ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,7 +26,14 @@ class LegalSupportActivity : AppCompatActivity() {
         binding = ActivityLegalSupportBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         binding.btnSubmitCase.setOnClickListener{
+
+            progressDialog = ProgressDialog(this)
+            progressDialog.setMessage("Reporting case.....")
+            progressDialog.setCancelable(false)
+            progressDialog.show()
+
 
             val hospName = binding.etHospName.text.toString()
             val caseType = binding.etTypeOfCase.text.toString()
@@ -34,16 +45,32 @@ class LegalSupportActivity : AppCompatActivity() {
 
             val LegalCase = LegalCase(hospName,caseType,dateOccurrence,involvedPerson,briefDesc)
 
-//            database.child()
+            database.child(hospName).setValue(LegalCase).addOnSuccessListener {
 
-            binding.etHospName.text.clear()
-            binding.etTypeOfCase.text.clear()
-            binding.etDateOccurrence.text.clear()
-            binding.etVictimInvolved.text.clear()
-            binding.etVictimInvolved.text.clear()
-            binding.etBriefDesc.text.clear()
+                binding.etHospName.text.clear()
+                binding.etTypeOfCase.text.clear()
+                binding.etDateOccurrence.text.clear()
+                binding.etVictimInvolved.text.clear()
+                binding.etVictimInvolved.text.clear()
+                binding.etBriefDesc.text.clear()
 
-            Toast.makeText(this,"successfuly added to db....",Toast.LENGTH_LONG).show()
+                Toast.makeText(this,"successfully reported....",Toast.LENGTH_LONG).show()
+
+
+                Handler().postDelayed({
+                    progressDialog.dismiss()
+                },8000)
+
+                progressDialog.dismiss()
+
+            }.addOnFailureListener {
+
+                Toast.makeText(this,"Failed. Try again..",Toast.LENGTH_LONG).show()
+                progressDialog.dismiss()
+
+            }
+
+
         }
 
 
