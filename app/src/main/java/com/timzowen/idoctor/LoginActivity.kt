c986542b6a054eb5f2618395ec67efc1f2e95098
TimzOwen <ktimothy.com>
2021-12-15 18:09:35 +0300

    upadated the activity

--- app/src/main/java/com/timzowen/idoctor/LoginActivity.kt
+++ app/src/main/java/com/timzowen/idoctor/LoginActivity.kt
@@ -1,11 +1,30 @@
 package com.timzowen.idoctor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.timzowen.idoctor.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+    private lateinit var dbRef : DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCallEmergency.setOnClickListener {
+            startActivity(Intent(this,EmergencyActivity::class.java))
+        }
+        binding.btnSignUp.setOnClickListener {
+            startActivity(Intent(this,SignUpActivity::class.java))
+        }
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
